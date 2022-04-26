d19d611169f3ea481ad9f705dcd7a634d6c92ffd
lies93 <hocini.lies@gmail.com>
2022-04-26 14:49:06 +0200

    setp 2.2 done

--- backend/index.js
+++ backend/index.js
@@ -25,35 +25,53 @@ const HeroesSchema = new mongoose.Schema({
 
 const HeroesModel = mongoose.model('heroes',HeroesSchema)
 
-HeroesModel.insertMany(
-    [
-        {
-                    slug: "iron-man",
-            name: "Iron Man",
-            power: ["money"],
-            color: "red",
-            isAlive: true,
-            age: 46,
-            image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
-        },
-        {
-                    slug: "thor",
-            name: "Thor",
-            power: ["electricty", "worthy"],
-            color: "blue",
-            isAlive: true,
-            age: 300,
-            image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
-        },
-        {
-                    slug: "dardevil",
-            name: "Daredevil",
-            power: ["blind"],
-            color: "red",
-            isAlive: false,
-            age: 30,
-            image: "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg"
-        }
-    ]    
-)
-console.log(HeroesModel)
+// HeroesModel.insertMany(
+//     [
+//         {
+//                     slug: "iron-man",
+//             name: "Iron Man",
+//             power: ["money"],
+//             color: "red",
+//             isAlive: true,
+//             age: 46,
+//             image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
+//         },
+//         {
+//                     slug: "thor",
+//             name: "Thor",
+//             power: ["electricty", "worthy"],
+//             color: "blue",
+//             isAlive: true,
+//             age: 300,
+//             image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
+//         },
+//         {
+//                     slug: "dardevil",
+//             name: "Daredevil",
+//             power: ["blind"],
+//             color: "red",
+//             isAlive: false,
+//             age: 30,
+//             image: "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg"
+//         }
+//     ])//.exec()
+    // .then(res=>{
+    //     console.log(res)
+    // })
+    
+
+app.get('/heroes', async function (req, res, next ){
+    const heroes= await HeroesModel.find({}).exec()
+    console.log("console log verif heroes",heroes)
+    res.json(heroes)
+    // console.log("console log de res app",res)
+})
+
+app.get('/heroes/:slug', function(req, res, next){
+    const slug = req.params.slug
+    console.log('console log de slug',slug)
+    const heroes = HeroesModel.find({slug}).exec().then((heroes)=>{
+        console.log("console log de something",heroes)
+        res.json(heroes)
+    })
+})
